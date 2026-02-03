e563e1cb3c34e76ac2361cd0275767ae56d6aa5b
Rahul kumar kushwaha <rahulkumar1kushwaha@gmail.com>
2026-02-03 20:36:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+# num=int(input("Ente number:"))
+# rev=0
+# while num>0:
+#     digit = num%10
+#     rev = rev * 10 + digit
+#     num = num//10
+# print("Rverse num=",rev)
+
+# a =int(input("Enter first number:"))
+# b =int(input("Enter second number:"))
+# sum = a+b
+# print(sum)
+
+# a =int(input("Enter first number:"))
+# b =int(input("Enter second number:"))
+# c =int(input("Enter third number:"))
+# if a>b and a>c:
+#     print("larger is a")
+# elif b>c:
+#     print("larger is b")
+# else:
+#     print("larger is c")
+
+# a = input("Enter the name:")
+# while True:
+#     if a =="xyz":
+#         print("ok")
+    
+    
+# i = 1
+# while i<=10:
+#     print(i)
+#     i=i+1
